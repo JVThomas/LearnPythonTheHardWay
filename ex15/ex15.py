4772de19a1f105cd57cfc603087f4a3fd9ee477c
Justin Thomas <jthomas0209@gmail.com>
2016-08-11 13:47:49 -0400

    Couple more python examples done

--- ex15/ex15.py
+++ ex15/ex15.py
@@ -6,12 +6,5 @@ txt = open(filename) #open the file, returns the file objext
 
 print "Here is your file %r" % filename #printing out the filename
 print txt.read() #reads the returned file object
+txt.close() # close the file when you're done with it
 
-#repeat with a new filename
-print "Type the filename again"
-new_filename = raw_input('>')
-
-txt_again = open(new_filename)
-
-print "Here is your file %r" % new_filename
-print txt_again.read()
